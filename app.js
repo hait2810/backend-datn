7d60c80a1c6b800482c7e684920c0d4baf21354f
Hai <checked.kenvip10@gmail.com>
2022-09-21 11:13:23 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+import  express  from "express";
+import mongoose from "mongoose";
+import cors from 'cors'
+import routerHome from "./routes/home";
+
+
+const url =
+  "mongodb+srv://datn_433:tg7aERk5yF9Jes9V@atlascluster.nyvzdzm.mongodb.net/?retryWrites=true&w=majority";
+async function connect() {
+  try {
+    await mongoose.connect(url);
+    console.log("connect succsess");
+  } catch (error) {
+    console.log(error);
+  }
+}
+connect();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(routerHome)
+
+
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("Đang chạy cổng", PORT);
+});
