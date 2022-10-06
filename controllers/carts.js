8ae63ff238b8e82dabcba0b75c81bbba0a6bc8d0
Hai <checked.kenvip10@gmail.com>
2022-10-06 09:06:59 +0700

    fix: Add New Order

--- controllers/carts.js
+++ controllers/carts.js
@@ -3,7 +3,7 @@ import Cart from '../models/carts'
 
 export const addNewOrder = async (req,res) => {
         try {
-            const cart = await Cart(req,res).save();
+            const cart = await Cart(req.body).save();
             res.json(cart)
         } catch (error) {
             res.status(400).json({message: error})
