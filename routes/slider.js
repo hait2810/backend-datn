8da9d20eff9fff4477d6050219840a88e14e82aa
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-10-06 17:37:24 +0700

    feature: Update Slider

--- routes/slider.js
+++ routes/slider.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createSliders, listSliders, readSliders } from "../controllers/slider";
+import {
+  createSliders,
+  listSliders,
+  readSliders,
+  removeSliders,
+} from "../controllers/slider";
 
 const router = express.Router();
 
 router.post("/slider", createSliders);
 router.post("/sliders", listSliders);
 router.get("/slider/:id", readSliders);
+router.delete("/slider/:id", removeSliders);
 
 export default router;
