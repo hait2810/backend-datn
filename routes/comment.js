b16af973262d20b5dc09a05d9dfd364edba08e17
caotruong0511 <truongcmph15429@fpt.edu.vn>
2022-11-16 22:43:07 +0700

    feature comment product

---
+++ routes/comment.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { isAdmin, isAuth, requireSignin } from "../middlewares/checkAuth";
+
+import {
+  create,
+  getByProduct,
+  list,
+  read,
+  remove,
+  update,
+} from "../controllers/comment";
+
+const router = express.Router();
+
+router.get("/comment", list);
+router.get("/comment/getByProduct/:id", getByProduct);
+router.get("/comment/:id", read);
+router.delete("/comment/:id", requireSignin, isAuth, remove);
+router.post("/comment", requireSignin, isAuth, create);
+router.put("/comment/:id", requireSignin, isAuth, update);
+
+export default router;
